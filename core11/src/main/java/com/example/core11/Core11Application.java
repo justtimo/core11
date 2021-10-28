a7728d36192e771cadbb574abc69d488a43cb9c1
justtimo <wby6225104@163.com>
2021-10-28 23:47:37 +0800

    init

---
+++ core11/src/main/java/com/example/core11/Core11Application.java
@@ -0,0 +1,13 @@
+package com.example.core11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Core11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Core11Application.class, args);
+    }
+
+}
---
+++ core11/src/test/java/com/example/core11/Core11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.core11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Core11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
